60bde56edc0d32b32331edb3dbd4d16800942124
Emptea <aryas1725@gmail.com>
2024-08-30 16:32:00 +0300

    minor fixes to adc_ex

--- code/Src/adc_ex.h
+++ code/Src/adc_ex.h
@@ -2,10 +2,7 @@
 #define __ADC_EX_H__
 #include "stm32h7xx_ll_adc.h"
 #include "stm32h7xx_ll_dma.h"
-
-#define RE_SG_LEN 1024//64000
-
-extern int16_t adc_buf[RE_SG_LEN];
+#include "protocol.h"
 
 void adc_calibration();
 void adc_en();

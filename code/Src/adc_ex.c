9ebbb2f72c030812dc1f7d1ea6adbae6c06d36f1
Emptea <aryas1725@gmail.com>
2024-09-11 16:00:56 +0300

    adc buf overflow fixed

--- code/Src/adc_ex.c
+++ code/Src/adc_ex.c
@@ -34,7 +34,7 @@ void adc_dma_config()
         LL_ADC_DMA_GetRegAddr(ADC3, LL_ADC_DMA_REG_REGULAR_DATA),
         (uint32_t)&sg,
         LL_DMA_GetDataTransferDirection(DMA1, LL_DMA_STREAM_0));
-    LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_0, 2 * RE_SG_LEN);
+    LL_DMA_SetDataLength(DMA1, LL_DMA_STREAM_0, RE_SG_LEN);
 
     LL_DMA_ClearFlag_TE0(DMA1);
     LL_DMA_EnableIT_TE(DMA1, LL_DMA_STREAM_0);

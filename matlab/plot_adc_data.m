d33a11fd9ebdfb5bbf49b849c25d1ed10db0010e
Emptea <aryas1725@gmail.com>
2024-11-29 13:45:47 +0300

    matlab test upd

--- matlab/plot_adc_data.m
+++ matlab/plot_adc_data.m
@@ -1,8 +1,8 @@
 close all
 clear s
-s = serialport("COM2",256000,"Timeout",50);
+s = serialport("COM3",256000,"Timeout",50);
 sg_len = 1024;
-fft_len = 32768;
+fft_len = 1024;
 Fs = 2e6;
 cmd_list = ["cmd0", ...
     "cmd1", ...
@@ -35,32 +35,37 @@ cfg = struct('poly',0x8005,'init',0xffff,'refin',1,'refout',1,'xorout',0);
 %%
 send_cmd(s, cmd.ping, arg.on, cfg)
 pack = read(s, 10, 'char');
-fprintf("cmd:%s, arg:%d\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'uint32'))
+print_ans(pack)
 %%
 send_cmd(s, cmd.pwr_on_off, arg.on, cfg)
 pack = read(s, 10, 'char');
-fprintf("cmd:%s, arg:%d\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'uint32'))
+print_ans(pack)
 %%
 send_cmd(s, cmd.set_sg_len, sg_len, cfg)
 pack = read(s, 10, 'char');
-fprintf("cmd:%s, arg:%d\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'uint32'))
+print_ans(pack)
+%%
+send_cmd(s, cmd.set_fft_len, sg_len, cfg)
+pack = read(s, 10, 'char'); 
+print_ans(pack)
 %%
 send_cmd(s, cmd.exti_on_off, arg.on, cfg);
 pack = read(s, 10, 'char');
-fprintf("cmd:%s, arg:%d\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'uint32'))
+print_ans(pack)
 pack = read(s, 10, 'char');
-fprintf("cmd:%s, arg:%.4f\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'single'))
+fprintf("cmd:%s, arg:%.4f\n\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'single'))
 crc2check = crc16(pack, cfg);
 if (crc2check == 0 & pack(1:4) == reverse('rslt'))
     v_max = typecast(uint8(pack(5:8)), 'single')
 end
+pause(1)
 %%
 send_cmd(s, cmd.send_sg, sg_len, cfg);
 pack = read(s, 10, 'char'); 
-fprintf("cmd:%s, arg:%d\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'uint32'))
+print_ans(pack)
 sg_pack = read(s, sg_len*2 + 2, 'char');
 crc2check = crc16(sg_pack, cfg);
-if (crc2check == 0 & pack(1:4) == '8dmc')
+if (crc2check == 0 & pack(1:4) == 'gisp')
     sg = typecast(uint8(sg_pack(1:end-2)), 'uint16');
     volt = sg/double(intmax("uint16"))*vref;
     figure; plot(volt)
@@ -70,11 +75,13 @@ if (crc2check == 0 & pack(1:4) == '8dmc')
 end
 
 %%
-send_cmd(s, fft_len, cfg);
-pack = read(s, 10 + fft_len*8 + 2, 'char');
+send_cmd(s, cmd.send_fft, fft_len, cfg);
+pack = read(s, 10, 'char');
+print_ans(pack)
+fft_pack = read(s, fft_len*8 + 2, 'char');
 crc2check = crc16(pack, cfg);
-if (crc2check == 0 & pack(1:4) == '8dmc')
-    data = typecast(uint8(pack(5:end-2)), 'single');
+if (crc2check == 0 & pack(1:4) == 'tffp')
+    data = typecast(uint8(fft_pack(1:end-2)), 'single');
     sg_fft = data(1:2:end) + 1i*data(2:2:end);
     plot_fft(sg_fft, fft_len, Fs)
     legend('stm32')
@@ -101,6 +108,11 @@ function send_cmd(s, cmd, arg, cfg)
     crc = crc16([char(cmd) arg_uint32], cfg);
     
     pack = [char(cmd) arg_uint32 typecast(uint16(crc), 'uint8')];
-    fprintf("cmd pack\ncmd:%s, arg:%d\n\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'uint32'))
+    fprintf("cmd pack\ncmd:%s, arg:%d\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'uint32'))
     write(s, pack, 'char');
+end
+
+%%
+function print_ans(pack)
+    fprintf("answer\ncmd:%s, arg:%d\n\n", reverse(pack(1:4)),typecast(uint8(pack(5:8)),'uint32'))
 end
